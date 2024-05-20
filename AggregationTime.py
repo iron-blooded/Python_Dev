cc59c061136e45934c94f2a3002a149d1607d805
iron-blooded <ayss2@mail.ru>
2024-05-20 18:54:03 +0500

    + наполнение бота

--- AggregationTime.py
+++ AggregationTime.py
@@ -1,7 +1,7 @@
 import bson
 class AggregationTime:
     def __init__(self, location="./sampleDB/sample_collection.bson") -> None:
-        with open("./sampleDB/sample_collection.bson", "rb") as f:
+        with open(location, "rb") as f:
             self.dataset = bson.decode_all(f.read())
     
     def input(self, task: dict[str:str]) -> dict[str : (list | str)]:
--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-import AggregationTime
+from AggregationTime import AggregationTime
+import os
+import asyncio
+from aiogram import Bot, Dispatcher, types, executor
+from aiogram.utils import executor
+import json
+
+dp = Dispatcher(Bot(token=os.environ["tg_token"]))  # Создаем переменную бота
+
+
+@dp.message_handler(commands=["start"])  # Обработка команды start
+async def send_welcome(message: types.Message):
+    return await message.answer(f"Hi {message.from_user.mention}!")
+
+
+@dp.message_handler()
+async def echo_message(message: types.Message):  # Обработка входящих сообщений
+    try:
+        return await message.answer(
+            str(AggregationTime().input(json.loads(message.text)))
+        )
+    except Exception as e:
+        return await message.answer(f"Ошибка: {e}")
+
+
+executor.start_polling(dp)  # Запускаем бота
